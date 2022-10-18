32a0941ffc2a4138f2be6ae3ce47ca6a971cd4de
Onias da Rocha <onias.filho@gmail.com>
2022-10-18 16:49:03 -0400

    inserts github and google button on large screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -25,10 +25,27 @@ const LoginScreen = () => {
               <input type="text" />
               <div className="lower-form">
                 <div className="submit-button-wrapper">
-                  <button id="submit-button" type="submit"> Sign In</button>
+                  <button id="submit-button" type="submit"> Entrar</button>
                 </div>
                 <div className="forgot-password-password">
-                  <p className="forgot-password">forgot password</p>
+                  <p className="forgot-password">esqueci minha senha</p>
+                </div>
+              </div>
+              <div className="loginDivision">
+                <span className="inner">ou</span>
+              </div>
+              <div className="social-area">
+                <div className="col">
+                  <button className="gh social-button">
+                    <div className="social-icon-wrapper">
+                      <i className="fa-brands fa-github"></i>
+                    </div> Entrar com Github
+                  </button>
+
+                  <button className="google social-button">
+                    <div className="social-icon-wrapper"><i className="fa-brands fa-google"></i></div>
+                    Entrar com Google
+                  </button>
                 </div>
               </div>
             </div>
